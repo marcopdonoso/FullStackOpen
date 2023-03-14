756a6a63aa87d7436c452b18d002b5bcd42ec4ef
Marco Perez Donoso <marcopdonoso@gmail.com>
2023-03-13 23:31:26 -0400

    Exercise 1.9

--- part1/unicafe/src/index.js
+++ part1/unicafe/src/index.js
@@ -21,6 +21,10 @@ const Positive = (props) => (
 );
 
 const Statistics = ({ good, neutral, bad }) => {
+  if (good === 0 && neutral === 0 && bad === 0) {
+    return <p>No feedback given</p>;
+  }
+
   return (
     <div>
       <p>good {good}</p>
