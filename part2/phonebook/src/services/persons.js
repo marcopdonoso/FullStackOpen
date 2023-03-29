06faacb41c23054fbfdbb27c5784ee2d4379f7e0
Marco Perez Donoso <marcopdonoso@gmail.com>
2023-03-29 13:52:44 -0400

    add proxy for backend server

--- part2/phonebook/src/services/persons.js
+++ part2/phonebook/src/services/persons.js
@@ -1,28 +1,28 @@
 import axios from "axios";
 
-const baseUrl = "http://localhost:3001/persons";
+const baseUrl = "/api/persons";
 
 const getAll = () => {
-  const request = axios.get(baseUrl);
+	const request = axios.get(baseUrl);
 
-  return request.then((response) => response.data);
+	return request.then((response) => response.data);
 };
 
 const create = (newPerson) => {
-  const request = axios.post(baseUrl, newPerson);
+	const request = axios.post(baseUrl, newPerson);
 
-  return request.then((response) => response.data);
+	return request.then((response) => response.data);
 };
 
 const deletePerson = (id) => {
-  const request = axios.delete(`${baseUrl}/${id}`);
+	const request = axios.delete(`${baseUrl}/${id}`);
 
-  return request.then();
+	return request.then();
 };
 
 const update = (id, newObject) => {
-  const request = axios.put(`${baseUrl}/${id}`, newObject);
+	const request = axios.put(`${baseUrl}/${id}`, newObject);
 
-  return request.then((response) => response.data);
+	return request.then((response) => response.data);
 };
 export default { getAll, create, deletePerson, update };
